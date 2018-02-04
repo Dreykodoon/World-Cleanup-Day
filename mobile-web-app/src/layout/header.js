524c24962a9af5f7190d67860643bdbd79f8ada5
Mihai Cioban <ciobanmihai@gmail.com>
2018-02-04 20:34:10 +0200

    mobile-web-app: camera view positioned correctly
    Note: it only works when holding the mobile phone vertically!

--- mobile-web-app/src/layout/header.js
+++ mobile-web-app/src/layout/header.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+const styles = {
+    header: {
+        height: '50px',
+    }
+}
+
 class Header extends Component {
     render() {
         return (
-            <div>
+            <div style={styles.header}>
                 <Link style={{display: 'inline-block'}} to='/camera'>Camera</Link>
                 <div style={{width: '15px', display: 'inline-block'}}></div>
                 <Link style={{display: 'inline-block'}} to='/gallery'>Gallery</Link>
