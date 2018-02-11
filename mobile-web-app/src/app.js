65092bbcde8707d1373fc4b135798d8bc9e2e89c
Mihai Cioban <ciobanmihai@gmail.com>
2018-02-11 18:01:33 +0200

    mobile-web-app: moved FB sdk loading to separate component

--- mobile-web-app/src/app.js
+++ mobile-web-app/src/app.js
@@ -5,9 +5,8 @@ import { connect } from 'react-redux';
 import Layout from './layout/layout';
 import Camera from './photo/camera';
 import Gallery from './photo/gallery';
+import FacebookSdkLoader from './authentication/facebook-sdk-loader';
 import { loadPhotos } from './photo/photo-actions';
-import { setFBLoginStatus } from './authentication/auth-actions';
-import { facebookInitialized } from './globals/globals-actions';
 
 
 class App extends Component {
@@ -15,64 +14,30 @@ class App extends Component {
         this.props.loadPhotos();
     }
 
-    componentDidMount() {
-        if (document.getElementById('facebook-jssdk')) {
-            return;
-        }
-        this.setFbAsyncInit();
-        this.loadFacebookSdkAsynchronously('script', 'facebook-jssdk');
-    }
-
-    setFbAsyncInit() {
-        window.fbAsyncInit = () => {
-            window.FB.init({
-                appId      : '155260651860400',
-                cookie     : true, // enable cookies to allow the server to access the session
-                xfbml      : true, // parse social plugins on this page
-                version    : 'v2.8' // use graph api version 2.8
-            });
-            this.props.facebookInitialized();
-            this.props.setFBLoginStatus();
-        };
-    }
-
-    loadFacebookSdkAsynchronously(elementTag, elementId) {
-        const facebookElement = document.getElementsByTagName(elementTag)[0];
-        if (document.getElementById(elementId)) {
-            return;
-        }
-        const element = document.createElement(elementTag);
-        element.id = elementId;
-        element.src = 'https://connect.facebook.net/en_US/sdk.js';
-        facebookElement.parentNode.insertBefore(element, facebookElement);
-    }
-
     render() {
         return (
-            <Router>
-                <Layout>
-                    <Switch>
-                        <Route exact path='/camera' component={Camera}/>
-                        <Route exact path='/gallery' component={Gallery}/>
-                        <Redirect to='/camera'/>
-                    </Switch>
-                </Layout>
-            </Router>
+            <FacebookSdkLoader>
+                <Router>
+                    <Layout>
+                        <Switch>
+                            <Route exact path='/camera' component={Camera}/>
+                            <Route exact path='/gallery' component={Gallery}/>
+                            <Redirect to='/camera'/>
+                        </Switch>
+                    </Layout>
+                </Router>
+            </FacebookSdkLoader>
         );
     }
 }
 
 App.propTypes = {
     loadPhotos: PropTypes.func,
-    setFBLoginStatus: PropTypes.func,
-    facebookInitialized: PropTypes.func,
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
         loadPhotos: () => dispatch(loadPhotos()),
-        setFBLoginStatus: () => dispatch(setFBLoginStatus()),
-        facebookInitialized: () => dispatch(facebookInitialized())
     };
 };
 
---
+++ mobile-web-app/src/authentication/facebook-sdk-loader.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { setFBLoginStatus } from './auth-actions';
+import { facebookInitialized } from '../globals/globals-actions';
+
+class FacebookSdkLoader extends React.Component {
+    componentDidMount() {
+        if (document.getElementById('facebook-jssdk')) {
+            return;
+        }
+        this.setFbAsyncInit();
+        this.loadFacebookSdkAsynchronously('script', 'facebook-jssdk');
+    }
+
+    setFbAsyncInit() {
+        window.fbAsyncInit = () => {
+            window.FB.init({
+                appId      : '155260651860400',
+                cookie     : true, // enable cookies to allow the server to access the session
+                xfbml      : true, // parse social plugins on this page
+                version    : 'v2.8' // use graph api version 2.8
+            });
+            this.props.facebookInitialized();
+            this.props.setFBLoginStatus();
+        };
+    }
+
+    loadFacebookSdkAsynchronously(elementTag, elementId) {
+        const facebookElement = document.getElementsByTagName(elementTag)[0];
+        if (document.getElementById(elementId)) {
+            return;
+        }
+        const element = document.createElement(elementTag);
+        element.id = elementId;
+        element.src = 'https://connect.facebook.net/en_US/sdk.js';
+        facebookElement.parentNode.insertBefore(element, facebookElement);
+    }
+
+    render() {
+        return this.props.children;
+    }
+}
+
+FacebookSdkLoader.propTypes = {
+    children: PropTypes.any,
+    setFBLoginStatus: PropTypes.func,
+    facebookInitialized: PropTypes.func,
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setFBLoginStatus: () => dispatch(setFBLoginStatus()),
+        facebookInitialized: () => dispatch(facebookInitialized())
+    };
+};
+
+export default connect(undefined, mapDispatchToProps)(FacebookSdkLoader);
