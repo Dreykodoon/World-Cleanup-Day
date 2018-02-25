e9605917f31ca3853e825d7683fe469d301161a5
Mihai Cioban <ciobanmihai@gmail.com>
2018-02-25 10:31:07 +0200

    mobile-web-app: messaging system in the works p1

--- mobile-web-app/src/layout/layout.js
+++ mobile-web-app/src/layout/layout.js
@@ -1,16 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import Snackbar from 'material-ui/Snackbar';
 import LoginScreen from './login-screen';
 import MainScreen from './main-screen';
+import { clearMessage } from '../globals/globals-actions';
 
-const Layout = () => (
+const Layout = ({message, hideMessage}) => (
     <div style={{height: '100%'}}>
         <Switch>
             <Route path='/login' component={LoginScreen}/>
             <Route path='/main' component={MainScreen}/>
             <Redirect to='/login'/>
         </Switch>
+        <Snackbar
+            anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+            open={message !== null}
+            onClose={hideMessage}
+            message={<div>{message}<button onClick={hideMessage}>close</button></div>}
+            autoHideDuration={5000}/>
     </div>
 );
 
-export default Layout;
+Layout.propTypes = {
+    message: PropTypes.string,
+    hideMessage: PropTypes.func,
+};
+
+const mapStateToProps = (state) => {
+    return {
+        message: state.globals.message,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        hideMessage: () => dispatch(clearMessage()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Layout);
